82db5539795d737e5c4e80ab9da6d30e47a39a0f
origin <khaiminh0003@gmail.com>
2024-05-08 09:59:16 +0700

    Test Code for Jenkins

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
+public class GradeConverterTest {
+    private GradeConverter converter;
+    @Before
+    public void setUp() throws Exception {
+        converter = new GradeConverter();
+    }
+
+    @Test
+    public void testConvert1() {
+        String expected = "A";
+        String actual = converter.convert(100);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public  void setConverter2 () {
+        String expected = "A";
+        String actual = converter.convert(90);
+        assertEquals(expected,actual);
+    }
+}
